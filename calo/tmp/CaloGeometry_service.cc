f9722957e0cf829b2dcbf28501e9bc09f01eaf8d
Brendan <kiburg@fnal.gov>
2013-11-27 00:03:19 -0600

    Moved the calogeometry_service to a tmp directory. did not work with the new art version. need to figure this out later

--- calo/tmp/CaloGeometry_service.cc
+++ calo/tmp/CaloGeometry_service.cc
@@ -4,11 +4,13 @@
 #include "gm2geom/Core/Geometry_service.hh"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 
-gm2geom::CaloGeometryService::CaloGeometryService(fhicl::ParameterSet const&, art::ActivityRegistry& iReg) :
+gm2geom::CaloGeometryService::CaloGeometryService(fhicl::ParameterSet const&, art::ActivityRegistry& ) :  //iReg) :
   geom_(new gm2geom::CalorimeterGeometry("calorimeter"))
 {
-	art::ServiceHandle <gm2geom::GeometryService> g; // ensure that GeometryService gets constructed first
-    iReg.watchPreBeginRun(this, &CaloGeometryService::preBeginRun);
+  //FIXME watchPreBeginRun not part of art 1.08.09
+  // need to figure this out
+  //art::ServiceHandle <gm2geom::GeometryService> g; // ensure that GeometryService gets constructed first
+  //iReg.watchPreBeginRun(this, &CaloGeometryService::preBeginRun);
 }
 
 void gm2geom::CaloGeometryService::update() {
