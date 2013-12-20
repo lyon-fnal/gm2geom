d2ffd7aa0719606fdb2aef9f3f0a749b0051e1af
Brendan <kiburg@fnal.gov>
2013-12-20 14:46:12 -0600

    new function call to sPreBeginRun.watch(...)

--- calo/CaloGeometry_service.cc
+++ calo/CaloGeometry_service.cc
@@ -4,14 +4,12 @@
 #include "gm2geom/Core/Geometry_service.hh"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 
-gm2geom::CaloGeometryService::CaloGeometryService(fhicl::ParameterSet const&, art::ActivityRegistry& ) : // iReg) :
+gm2geom::CaloGeometryService::CaloGeometryService(fhicl::ParameterSet const&, art::ActivityRegistry& iReg) :
   geom_(new gm2geom::CalorimeterGeometry("calorimeter"))
 {
-  //FIXME watchPreBeginRun not part of art 1.08.09
-  // need to figure this out
+
   art::ServiceHandle <gm2geom::GeometryService> g; // ensure that GeometryService gets constructed first
-  //iReg.sPreBeginRun.watch(&CaloGeometryService::preBeginRun);
-  //iReg.watchPreBeginRun(this, &CaloGeometryService::preBeginRun);
+  iReg.sPreBeginRun.watch(this,&CaloGeometryService::preBeginRun);
 }
 
 void gm2geom::CaloGeometryService::update() {
